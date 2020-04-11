45b2b25d28bfd56adb44f117781dc00bca3c74f1
Tharwat Abou-Helal <tharwat96@gmail.com>
2020-04-11 10:33:44 +0200

    Update provider.tf

--- aws/provider.tf
+++ aws/provider.tf
@@ -2,8 +2,8 @@
 provider "aws" {
   profile    = "aws"
   region     = "${var.AWS_REGION}"
-  access_key = "${var.jenkins_aws_secret_key_id}"
-  secret_key = "${var.jenkins_aws_secret_access_key}"
+  # access_key = "${var.jenkins_aws_secret_key_id}"
+  # secret_key = "${var.jenkins_aws_secret_access_key}"
 
 }
 
